5c1b4813d64a550295e235e15bb439ee12ed9d18
Frank R. Ray <fray@ffg.com>
2014-02-19 12:12:04 -0500

    Maybe a little improved...

--- player.rb
+++ player.rb
@@ -1,67 +1,29 @@
 class Player
+
+  attr_reader :surrounding_spaces, :nearby_enemies, :nearby_captives, :warrior
+  
   def play_turn(warrior)
 
     @surrounding_spaces = [:forward, :backward, :right, :left]
     @warrior = warrior
-    @health = warrior.health
-    @nearby_enemies = detect_enemies
-    @nearby_captives = detect_captives
+    @nearby_enemies = surrounding_spaces.find_all {|direction| warrior.feel(direction).enemy?}
+    @nearby_captives = surrounding_spaces.find_all {|direction| warrior.feel(direction).captive?}
 
-    if enemy_close?
-      if multiple_enemies?
+    if nearby_enemies.count > 0
+      if nearby_enemies.count > 1
           warrior.bind! closest_enemy
       else
-          warrior.attack! closest_enemy
+          warrior.attack! nearby_enemies.first
       end
-    elsif injured?
+    elsif warrior.health < 20
       warrior.rest!
-    elsif captive_close?
-      warrior.rescue! closest_captive
-    elsif more_encounters?
-      warrior.walk! direction_of_encounter
+    elsif nearby_captives.count > 0
+      warrior.rescue! nearby_captives.first
+    elsif warrior.listen.count > 0
+      warrior.walk! warrior.direction_of(warrior.listen.first)
     else
       warrior.walk! warrior.direction_of_stairs
     end
-  end
-
-  def injured?
-    @health < 20
-  end
-
-  def enemy_close?
-    @nearby_enemies.count > 0
-  end
-
-  def multiple_enemies?
-    @nearby_enemies.count > 1
-  end
-  
-  def captive_close?
-    @nearby_captives.count > 0
-  end
-
-  def detect_enemies
-    @surrounding_spaces.find_all {|direction| @warrior.feel(direction).enemy? }
-  end
-
-  def detect_captives
-    @surrounding_spaces.find_all {|direction| @warrior.feel(direction).captive? }
-  end
-
-  def closest_enemy
-    @nearby_enemies.first
-  end
-
-  def closest_captive
-    @nearby_captives.first
-  end
-
-  def more_encounters?
-    @warrior.listen.count > 0
-  end
-
-  def direction_of_encounter
-    @warrior.direction_of(@warrior.listen.first)
   end
 
 end
